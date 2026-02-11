ff38b38e0f3622bb727d2dbc7f30c9b7ed475e5c
Ryan <ryanrana_4@hotmail.com>
2026-02-11 23:11:13 +0000

    Inital commits

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+});
+
+app.get("/test", (req, res) => {
+  res.json({ working: true });
+});
