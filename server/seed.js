709378eb98717a41a2f7ef00b3bb9f9d17c769bd
Ryan <ryanrana_4@hotmail.com>
2026-02-13 03:40:31 +0000

    Changed schema to incl composite unique for title+artist. Created seed.js to upsert some songs into db

---
+++ server/seed.js
@@ -0,0 +1,41 @@
+const prisma = require("./prismaClient");
+
+async function main() {
+  const songs = [
+    {
+      title: "I Miss You",
+      artist: "blink-182",
+    },
+    {
+      title: "Feeling This",
+      artist: "blink-182",
+    },
+    {
+      title: "Sleepwalking",
+      artist: "Bring Me The Horizon",
+    },
+    {
+      title: "Sleepwalking",
+      artist: "All Time Low",
+    },
+  ];
+
+  await Promise.all(
+    songs.map(song =>
+      prisma.song.upsert({
+        where: {
+          unique_song: {
+            title: song.title,
+            artist: song.artist,
+          },
+        },
+        update: {},
+        create: song,
+      }),
+    ),
+  );
+
+  console.log("Seed ran");
+}
+
+main();
