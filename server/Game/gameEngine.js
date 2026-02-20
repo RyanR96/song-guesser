6a04e5bd1cf6cacd9002af9f6dd3a51505918861
Ryan <ryanrana_4@hotmail.com>
2026-02-20 18:30:23 +0000

    Next round ends game if last song is reached. Also actually changes song and resets user guesses. Correct guesses give users points, wont let them guess if they already got the song in that round

--- server/Game/gameEngine.js
+++ server/Game/gameEngine.js
@@ -5,6 +5,8 @@ class GameEngine {
     this.currentRound = 0;
     this.totalRounds = 15;
     this.players = {};
+    this.correctGuessesThisRound = [];
+    this.roundStartTime;
   }
 
   startGame(songs) {
@@ -41,17 +43,31 @@ class GameEngine {
 
   submitGuess(username, guess) {
     if (!this.isPlaying) return { error: "Game not underway" };
+    if (this.correctGuessesThisRound.includes(username))
+      return { error: "You've already guessed correctly this round!" };
     if (guess.toLowerCase() === this.currentSong.title.toLowerCase()) {
-      return { correct: true };
+      this.correctGuessesThisRound.push(username);
+      let points = 10;
+      this.players[username].score += points;
+      // Add points based on time they got answer right, aka change it later
+
+      return { correct: true, score: this.players[username].score };
     }
 
     return { correct: false };
   }
 
   nextRound() {
+    this.correctGuessesThisRound = [];
     if (!this.songs) return false;
-    if (this.currentRound + 1 > this.songs.length) return false;
+    if (this.currentRound + 1 > this.songs.length) {
+      this.isPlaying = false;
+      return false;
+    }
+
     this.currentRound++;
+    this.currentSong = this.songs[this.currentRound - 1];
+    console.log("Current song: ", this.currentSong);
     return true;
   }
 
