0756763b7c8c522d9f5bee7ebf3b3ea5f03d74e0
Ryan <ryanrana_4@hotmail.com>
2026-03-19 00:22:51 +0000

    Added barebones homepage, with join game modal. Navigates user to gamepage if username valid

---
+++ client/src/Components/JoinModal.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function joinModal(props) {
+  const { onClose, onJoinSuccess } = props;
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted");
+
+    if (!username.trim()) {
+      setError("Please enter a username");
+      return;
+    }
+
+    console.log("You are:", username.trim());
+    onJoinSuccess();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border-2 border-black"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button
+          className="bg-green-600 hover:bg-green-500 px-8 py-3 rounded-lg font-semibold text-center"
+          type="submit"
+        >
+          Join as guest
+        </button>
+      </form>
+      {error ?? <p>{error}</p>}
+    </div>
+  );
+}
+
+export default joinModal;
--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import JoinModal from "../Components/JoinModal";
+
 function Homepage() {
+  const [isJoinOpen, setIsJoinOpen] = useState(false);
+  const navigate = useNavigate();
+
+  function handlePlayClick() {
+    setIsJoinOpen(true);
+  }
+
+  function handleJoinSuccess() {
+    setIsJoinOpen(false);
+    navigate("/game");
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Homepage!</h1>
+    <div className="items-center justify-center flex flex-col">
+      <h1 className="text-3xl font-bold underline text-center">
+        Song Guessing Game
+      </h1>
+
+      <p className="text-center">Join the lobby</p>
+      <button
+        className="bg-green-600 hover:bg-green-500 px-8 py-3 rounded-lg font-semibold text-center"
+        onClick={handlePlayClick}
+      >
+        Play game
+      </button>
+      {isJoinOpen && (
+        <JoinModal
+          onClose={() => setIsJoinOpen(false)}
+          onJoinSuccess={handleJoinSuccess}
+        />
+      )}
     </div>
   );
 }
