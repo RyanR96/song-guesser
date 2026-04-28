fda9d71292b2f683583364dce34da8a1647ca4ad
Ryan <ryanrana_4@hotmail.com>
2026-04-28 17:45:53 +0100

    Tracks and artists now get added to DB. If item is null, moveonto next item. At the end, logs how many items skipped.

--- server/importSpotifyTrack.js
+++ server/importSpotifyTrack.js
@@ -3,8 +3,9 @@ const prisma = require("./prismaClient");
 async function importPlaylist() {
   const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
   const playlistId = process.env.SPOTIFY_PLAYLIST_ID;
+  let skipped = 0;
 
-  let url = `https://api.spotify.com/v1/playlists/${playlistId}/items?limit=10&offset=830`;
+  let url = `https://api.spotify.com/v1/playlists/${playlistId}/items?limit=10&offset=0`;
   while (url) {
     const response = await fetch(url, {
       headers: {
@@ -22,13 +23,34 @@ async function importPlaylist() {
 
     for (const item of data.items) {
       const song = item.item;
-      const songName = song.name.replace(/\(.*?\)/g, "").trim();
+      if (!song) {
+        console.log("Item failed:", item);
+        skipped++;
+        continue;
+      }
+      const songName = song.name.replace(/\(.*?\)/g, "").trim(); //removes brackets and trims whitespace
       const songArtists = song.artists.map(artist => artist.name);
 
       console.log(songName + " - " + songArtists);
+
+      await prisma.song.upsert({
+        where: {
+          unique_song: {
+            title: songName,
+            artist: songArtists,
+          },
+        },
+        update: {},
+        create: {
+          title: songName,
+          artist: songArtists,
+        },
+      });
     }
 
     url = data.next;
   }
+
+  console.log("Skipped:", skipped);
 }
 importPlaylist();
