393919359b14fa6d4f3de335f29dd9669dbf6983
Ryan <ryanrana_4@hotmail.com>
2026-06-21 20:09:06 +0100

    Added helper js file, added levenshtein distance algorithm in it

---
+++ server/Game/matching.js
@@ -0,0 +1,32 @@
+function levenshteinDistance(a, b) {
+  const matrix = [];
+
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b[i - 1] === a[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1,
+          matrix[i][j - 1] + 1,
+          matrix[i - 1][j] + 1,
+        );
+      }
+    }
+  }
+  return matrix[b.length][a.length];
+}
+
+console.log(levenshteinDistance("duck", "dark")); // 2
+console.log(levenshteinDistance("wots my age agian", "whats my age again"));
+console.log(
+  levenshteinDistance("awhats amy aage aagain", "whats my age again"),
+);
