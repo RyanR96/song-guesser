edb717ae3b34436578f1fbcd8d184730dd1efa47
Ryan <ryanrana_4@hotmail.com>
2026-07-12 22:30:07 +0100

    Added new Profile page, empty content currently. Added it to my frontend Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Gamepage from "./Pages/Gamepage";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/game" element={<Gamepage />} />
+      <Route path="/profile/:username" element={<Profile />} />
     </Routes>
   );
 }
