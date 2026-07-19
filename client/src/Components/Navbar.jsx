70a3ce24f2e885aebb0b065c60ca6f3f759a069d
Ryan <ryanrana_4@hotmail.com>
2026-07-19 02:15:50 +0100

    Added the basics to navbar - home/play/myprofile buttons. Myprofile opens in new tab if user is in game

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,9 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+
 function Navbar(props) {
   const { currentUser, onAuthSuccess, onLogout } = props;
+
+  const location = useLocation();
+
+  const isGamePage = location.pathname === "/game";
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="flex items-center justify-between p-4 border-b border-black">
+      <div>
+        <Link to="/" className="font-bold">
+          Song Guessing Game
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Link to="/"> Home</Link>
+          <Link to="/game"> Play</Link>
+
+          {currentUser && (
+            <Link
+              to={`/profile/${currentUser.username}`}
+              target={isGamePage ? "_blank" : undefined}
+              rel={isGamePage ? "noopener noreferrer" : undefined}
+            >
+              My profile
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
 
